6eb3957d1af57e80a487f66c327c8ac16a935084
shakugannoshana <a381097999@qq.com>
2025-08-12 08:35:28 +0800

    feat(components): demo

---
+++ play/src/HeadlessApp.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- If you want to apply a custom namespace, uncomment the following and apply the css configuration in `vite.config.ts`. -->
+  <!-- <el-config-provider namespace="ep"> -->
+  <div class="play-container">
+    <!-- <Form>
+      <Field name="email" type="email" :rules="fieldSchema" />
+      <ErrorMessage name="email" />
+    </Form> -->
+    <HeadlessForm>
+      <HeadlessFormItem name="email" type="email" :rules="fieldSchema" />
+    </HeadlessForm>
+  </div>
+  <!-- </el-config-provider> -->
+</template>
+
+<script setup lang="ts">
+import { Field, Form, ErrorMessage } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as zood from 'zod'
+import { HeadlessForm, HeadlessFormItem } from '@shana/headless-form'
+const fieldSchema = toTypedSchema(
+  zood.string().nonempty('Field is required').email('Must be a valid email')
+)
+</script>
+
+<style lang="scss">
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+
+  #play {
+    height: 100%;
+    width: 100%;
+
+    .play-container {
+      height: 100%;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+</style>
